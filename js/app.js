f2de7614a1bcdd71fb90d468aca71e4640704de0
Alex <alexcannon2@gmail.com>
2016-06-01 11:26:36 -0400

    Buildlist with creating element

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 $(document).foundation();
 
 var buildColorDiv = function(color) {
+  if (color === undefined) {
+    color = 'red';
+  }
   return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>'
 };
 
-var buildList = function(listValues = {}) {
-  return ' \
-    <dl> \
-      <dt>Name</dt> \
-      <dd>' + listValues.name + '</dd> \
-          \
-      <dt>Hair Color</dt> \
-      <dd>' + listValues.hairColor + '</dd> \
-        \
-      <dt>Age</dt> \
-      <dd>' + listValues.age + '</dd> \
-        \
-      <dt>Birthplace</dt> \
-      <dd>' + listValues.birthplace + '</dd> \
-    </dl>';
+var buildList = function(listValues) {
+  var dl = document.createElement('dl');
+  dl.style.border = '1px solid #ff0033';
+  dl.appendChild(buildListItem('Name', listValues.name));
+  dl.appendChild(buildListItem('Hair Color', listValues.hairColor));
+  dl.appendChild(buildListItem('Age', listValues.age));
+  dl.appendChild(buildListItem('Birthplace', listValues.birthplace));
+  return dl;
+};
+
+var buildListItem = function(term, definition) {
+  var li = document.createElement('li');
+  var dt = document.createElement('dt');
+  var dd = document.createElement('dd');
+  dt.innerHTML = term;
+  dd.innerHTML = definition;
+  li.appendChild(dt);
+  li.appendChild(dd);
+  return li;
 };
 
 var addValuesToDetails = function(ev) {
@@ -31,11 +38,10 @@ var addValuesToDetails = function(ev) {
     hairColor: buildColorDiv(this.hairColor.value)
   };
 
-  details.innerHTML += buildList(formValues);
+  details.appendChild(buildList(formValues));
 };
 
 document.querySelector('form').onsubmit = addValuesToDetails;
-
 
 
 
