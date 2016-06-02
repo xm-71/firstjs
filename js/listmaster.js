b1c523e76b802bd7cf680d4d05982fcbf3184180
xm-71 <alexcannon2@live.com>
2016-06-02 11:46:57 -0400

    add remove link

--- js/listmaster.js
+++ js/listmaster.js
@@ -22,8 +22,25 @@ var megaRoster = {
 
   buildListItem: function(studentName) {
     var item = document.createElement('li');
+    var deleteLink = this.buildLink({
+      text: 'remove',
+      handler: function(ev){
+        var list = item.parentElement;
+        list.removeChild(item);
+      },
+    })
     item.innerText = studentName;
+    item.appendChild(deleteLink);
     return item;
+  },
+
+  buildLink: function(options){
+    var link = document.createElement('a');
+    link.href = '#';
+    link.innerText = options.text;
+    link.onclick = options.handler;
+    return link;
+
   },
 }
 
