f346c392ec76cedb16224c1c85df37e8c09c7db3
xm-71 <alexcannon2@live.com>
2016-06-02 20:41:17 -0400

    Unpromote link added & non working up & down added

--- js/listmaster.js
+++ js/listmaster.js
@@ -15,41 +15,76 @@ var megaRoster = {
     var studentName = f.studentName.value;
     var item = this.buildListItem(studentName);
     var list = document.querySelector('#studentList');
-    list.appendChild(item);
+    this.prependChild(list, item);
     f.reset();
     f.studentName.focus();
   },
 
+  prependChild: function(parent, child) {
+    parent.insertBefore(child, parent.firstChild);
+  },
+
   buildListItem: function(studentName) {
     var item = document.createElement('li');
+    item.innerText = studentName;
+
+    // BEGIN things to move to a separate function
     var deleteLink = this.buildLink({
       text: 'remove',
-      handler: function(ev){
+      handler: function(ev) {
         var list = item.parentElement;
         list.removeChild(item);
-      },
+      }
     });
 
     var promoteLink = this.buildLink({
       text: 'promote',
-      handler: function(ev){
-        item.style.border = '2px cornflowerBlue dashed';
-
-      },
+      handler: function(ev) {
+        item.style.border = '2px CornflowerBlue dashed';
+      }
     });
-    item.innerText = studentName;
+
+    var unPromoteLink = this.buildLink({
+      text: 'UnPromote',
+      handler: function(ev) {
+        item.style.border = '';
+      }
+    });
+
+    var MoveUpLink = this.buildLink({
+      text: 'Move Up',
+      handler: function(ev) {
+        parent.nextSibling();
+      }
+    });
+
+    var MoveDownLink = this.buildLink({
+      text: 'Move Dow ',
+      handler: function(ev) {
+        parent.previousSibling();
+      }
+    });
+
     item.appendChild(deleteLink);
     item.appendChild(promoteLink);
+    item.appendChild(unPromoteLink);
+    item.appendChild(MoveUpLink);
+    item.appendChild(MoveDownLink);
+    // END things to move to a separate function
+
     return item;
   },
 
-  buildLink: function(options){
+  appendLinks: function(item) {
+    // Append the delete and promote links
+  },
+
+  buildLink: function(options) {
     var link = document.createElement('a');
     link.href = '#';
     link.innerText = options.text;
     link.onclick = options.handler;
     return link;
-
   },
 }
 
